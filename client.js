30f0ad4e3eb6ab48c95270247630122b523d3ba8
Alice Lin <alicexxlin@gmail.com>
2023-06-07 20:37:44 +0000

    Update IP client to localhost

--- client.js
+++ client.js
@@ -4,7 +4,7 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: "10.0.2.15",
+    host: "localhost",
     port: "50541",
   });
 
