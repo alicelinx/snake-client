32fffa5c8d73513e3803a86c84cafb7b7ea4a943
Alice Lin <alicexxlin@gmail.com>
2023-06-08 01:20:16 +0000

    Create constants and export to input file

--- input.js
+++ input.js
@@ -1,3 +1,5 @@
+const { SEND_MESSAGE_KEY, MOVE_UP_KEY, MOVE_LEFT_KEY, MOVE_DOWN_KEY, MOVE_RIGHT_KEY } = require("./constants");
+
 let connection;
 
 // setup interface to handle user input from stdin
@@ -16,24 +18,24 @@ const handleUserInput = (key) => {
     process.exit();
   }
 
-  if (key === 'w') {
+  if (key === MOVE_UP_KEY) {
     connection.write("Move: up");
   }
 
-  if (key === 'a') {
+  if (key === MOVE_LEFT_KEY) {
     connection.write("Move: left");
   }
 
-  if (key === 's') {
+  if (key === MOVE_DOWN_KEY) {
     connection.write("Move: down");
   }
 
-  if (key === 'd') {
+  if (key === MOVE_RIGHT_KEY) {
     connection.write("Move: right");
   }
 
-  if (key === 'm') {
-    connection.write("Say: Hi!");
+  if (key === SEND_MESSAGE_KEY) {
+    connection.write("Say: Hey again");
   }
 };
 
